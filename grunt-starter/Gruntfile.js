7bf92d36aab24a4226d246895805dde0eb3747fe
xue.zeng <xue.to.zeng@gmail.com>
2016-06-21 23:48:45 +0800

    initialization

---
+++ grunt-starter/Gruntfile.js
@@ -0,0 +1,31 @@
+'use strict';
+
+module.exports = function (grunt) {
+
+    // Show elapsed time at the end
+    require('time-grunt')(grunt);
+
+    var _ = require('lodash');
+
+    // Global config object
+    var config = {
+        pkg: grunt.file.readJSON('package.json'),
+        settings: grunt.file.readJSON('conifg.json')
+    };
+
+    // Load task configs from `grunt` directory
+    _.extend(config,
+        require('load-grunt-config')(grunt, {
+            init: false,
+            // lazy load tasks using jit grunt - https://github.com/shootaroo/jit-grunt
+            jitGrunt: {
+                staticMappings: {
+                    sprite: 'grunt-spritesmith',
+                    rig: 'grunt-rigger'
+                }
+            }
+        })
+    );
+
+    grunt.initConfig(config);
+};
